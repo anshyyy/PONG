4bfbf832c11094e17b89ab17de4b389e181efa9d
anshyyy <anshuman9998@gmail.com>
2021-09-30 16:21:52 +0530

    MADE FINAL CHANGES

--- main.py
+++ main.py
@@ -1,8 +1,50 @@
 from turtle import Turtle, Screen
-
-screen = Screen();
-screen.setup(width=800, height=800)
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+screen = Screen()
+screen.setup(width=800, height=600)
 screen.bgcolor('black')
+screen.title('PONG')
+screen.tracer(0)
+
+r_paddle = Paddle((380, 0))
+l_paddle = Paddle((-380, 0))
+ball = Ball()
+score = Scoreboard()
+
+screen.listen()
+screen.onkey(r_paddle.go_up, 'Up')
+screen.onkey(r_paddle.go_down, 'Down')
+
+screen.onkey(l_paddle.go_up, 'W')
+screen.onkey(l_paddle.go_down, 'S')
+
+
+game_on = True
+while game_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    #detect collision with ball
+    if ball.ycor()>280 or ball.ycor()< -280:
+        ball.bounce_y()
+
+    #detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 340 or ball.distance(l_paddle) < 50 and ball.xcor() < -340:
+        ball.bounce_x()
+    #detect when right paddle misses
+    if ball.xcor() > 380:
+       ball.reset_pos()
+       score.l_point()
+
+
+    #detect when left paddle misses
+    if ball.xcor() < -380:
+        ball.reset_pos()
+        score.r_point()
 
 
 screen.exitonclick()
--- readme.md
+++ readme.md
@@ -2,3 +2,5 @@ Classic Pong game
 
 It's a part of my small projects that i've been making in 22nd Day of 100 days of 
 python
+
+![](C:/Users/hp/OneDrive/Pictures/Screenshots/Screenshot (55).png)
